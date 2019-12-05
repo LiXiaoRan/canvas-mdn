73283510d405ce8baf20af6f4f8743733e24a99a
997843911@qq.com <997843911@qq.com>
2019-12-05 16:01:22 +0800

    增加了渐变，阴影

--- src/CanvasComponent/ColorCanvas.js
+++ src/CanvasComponent/ColorCanvas.js
@@ -13,6 +13,9 @@ class ColorCanvas extends Component {
         this.canvasLwRef = React.createRef();
         this.canvasLcapRef = React.createRef();
         this.canvasLDRef = React.createRef();
+        this.canvasLGRef = React.createRef();
+        this.canvasPatternRef = React.createRef();
+        this.canvasShadowRef=React.createRef();
         this.draw = this.draw.bind(this);
         this.dashTimer = null;
     }
@@ -25,6 +28,9 @@ class ColorCanvas extends Component {
         this.drawLineCap("canvasLcapRef");
         // this.drawLineDash("canvasLDRef");
         this.march();
+        this.drawLinearGradient("canvasLGRef");
+        this.drawPattern("canvasPatternRef");
+        this.drawShadow("canvasShadowRef")
     }
 
     getCtx(ref) {
@@ -135,7 +141,7 @@ class ColorCanvas extends Component {
         ctx.clearRect(0, 0, 150, 150);
         ctx.setLineDash([4, 2]);
         ctx.lineDashOffset = -offset;
-        ctx.strokeRect(10, 10, 100, 100);
+        ctx.strokeRect(10, 10, 130, 130);
     }
 
     march() {
@@ -147,6 +153,53 @@ class ColorCanvas extends Component {
         this.dashTimer = setTimeout(this.march.bind(this), 20);
     }
 
+    drawLinearGradient(canvas) {
+        let ctx = this.getCtx(canvas);
+        // Create gradients
+        var lingrad = ctx.createLinearGradient(0, 0, 0, 150);
+        lingrad.addColorStop(0, "#00ABEB");
+        lingrad.addColorStop(0.5, "#fff");
+        lingrad.addColorStop(0.5, "#26C000");
+        lingrad.addColorStop(1, "#fff");
+
+        var lingrad2 = ctx.createLinearGradient(0, 50, 0, 95);
+        lingrad2.addColorStop(0.5, "#000");
+        lingrad2.addColorStop(1, "rgba(0,0,0,0)");
+
+        // assign gradients to fill and stroke styles
+        ctx.fillStyle = lingrad;
+        ctx.strokeStyle = lingrad2;
+
+        // draw shapes
+        ctx.fillRect(10, 10, 130, 130);
+        ctx.strokeRect(50, 50, 50, 50);
+    }
+
+    drawPattern(canvas) {
+        let ctx = this.getCtx(canvas);
+        let img = new Image();
+        img.src =
+            "https://mdn.mozillademos.org/files/222/Canvas_createpattern.png";
+
+        img.onload = function() {
+            // 创建图案
+            let ptrn = ctx.createPattern(img, "repeat");
+            ctx.fillStyle = ptrn;
+            ctx.fillRect(0, 0, 150, 150);
+        };
+    }
+
+    drawShadow(canvas) {
+        let ctx = this.getCtx(canvas);
+        ctx.shadowOffsetX = 2;
+        ctx.shadowOffsetY = 2;
+        ctx.shadowBlur = 2;
+        ctx.shadowColor = "rgba(0, 0, 0, 0.5)";
+
+        ctx.font = "20px Times New Roman";
+        ctx.fillStyle = "Black";
+        ctx.fillText("Sample String", 5, 30);
+    }
     componentDidMount() {
         this.draw();
     }
@@ -211,9 +264,39 @@ class ColorCanvas extends Component {
                 </Card>
 
                 <Card title="lineDash 虚线  " className="wrap_card">
-                    <p>同样长度但是不同lineCap的线条</p>
+                    <p>虚线动画</p>
                     <canvas
                         ref={this.canvasLDRef}
+                        id="tutorial"
+                        width="150"
+                        height="150"
+                    ></canvas>
+                </Card>
+
+                <Card title="createLinearGradient 的例子" className="wrap_card">
+                    <p></p>
+                    <canvas
+                        ref={this.canvasLGRef}
+                        id="tutorial"
+                        width="150"
+                        height="150"
+                    ></canvas>
+                </Card>
+
+                <Card title="createPattern 的例子" className="wrap_card">
+                    <p></p>
+                    <canvas
+                        ref={this.canvasPatternRef}
+                        id="tutorial"
+                        width="150"
+                        height="150"
+                    ></canvas>
+                </Card>
+
+                <Card title="shadow" className="wrap_card">
+                    <p></p>
+                    <canvas
+                        ref={this.canvasShadowRef}
                         id="tutorial"
                         width="150"
                         height="150"
--- src/CanvasComponent/README.md
+++ src/CanvasComponent/README.md
@@ -255,3 +255,71 @@ miterLimit 属性就是用来设定外延交点与连接点的最大距离，如
 
 ## 使用虚线
 用 `setLineDash` 方法和 `lineDashOffset` 属性来制定虚线样式. `setLineDash` 方法接受一个数组，来指定线段与间隙的交替；`lineDashOffset` 属性设置起始偏移量.
+
+## 渐变 Gradients
+渐变 Gradients
+就好像一般的绘图软件一样，我们可以用线性或者径向的渐变来填充或描边。我们用下面的方法新建一个 canvasGradient 对象，并且赋给图形的 `fillStyle` 或 `strokeStyle` 属性。
+
+- `createLinearGradient(x1, y1, x2, y2)`
+createLinearGradient 方法接受 4 个参数，表示渐变的起点 (x1,y1) 与终点 (x2,y2)。
+- `createRadialGradient(x1, y1, r1, x2, y2, r2)`
+createRadialGradient 方法接受 6 个参数，前三个定义一个以 (x1,y1) 为原点，半径为 r1 的圆，后三个参数则定义另一个以 (x2,y2) 为原点，半径为 r2 的圆。
+```js
+var lineargradient = ctx.createLinearGradient(0,0,150,150);
+var radialgradient = ctx.createRadialGradient(75,75,0,75,75,100);
+```
+
+创建出 canvasGradient 对象后，我们就可以用 addColorStop 方法给它上色了。
+
+- `gradient.addColorStop(position, color)`
+addColorStop 方法接受 2 个参数，position 参数必须是一个 0.0 与 1.0 之间的数值，表示渐变中颜色所在的相对位置。例如，0.5 表示颜色会出现在正中间。color 参数必须是一个有效的 CSS 颜色值（如 #FFF， rgba(0,0,0,1)，等等）。
+你可以根据需要添加任意多个色标（color stops）。下面是最简单的线性黑白渐变的例子。
+```js
+var lineargradient = ctx.createLinearGradient(0,0,150,150);
+lineargradient.addColorStop(0,'white');
+lineargradient.addColorStop(1,'black');
+```
+
+
+## 图案样式 Patterns
+
+- `createPattern(image, type)`
+该方法接受两个参数。Image 可以是一个 Image 对象的引用，或者另一个 canvas 对象。Type 必须是下面的字符串值之一：`repeat，repeat-x，repeat-y 和 no-repeat`。
+
+图案的应用跟渐变很类似的，创建出一个 pattern 之后，赋给 fillStyle 或 strokeStyle 属性即可。
+
+```js
+var img = new Image();
+img.src = 'someimage.png';
+var ptrn = ctx.createPattern(img,'repeat');
+```
+
+> 注意：与 drawImage 有点不同，你需要确认 image 对象已经装载完毕，否则图案可能效果不对的。
+
+## 阴影 Shadows 
+- shadowOffsetX = float
+shadowOffsetX 和 shadowOffsetY 用来设定阴影在 X 和 Y 轴的延伸距离，它们是不受变换矩阵所影响的。负值表示阴影会往上或左延伸，正值则表示会往下或右延伸，它们默认都为 0。
+
+- shadowOffsetY = float
+shadowOffsetX 和 shadowOffsetY 用来设定阴影在 X 和 Y 轴的延伸距离，它们是不受变换矩阵所影响的。负值表示阴影会往上或左延伸，正值则表示会往下或右延伸，它们默认都为 0。
+- shadowBlur = float
+shadowBlur 用于设定阴影的模糊程度，其数值并不跟像素数量挂钩，也不受变换矩阵的影响，默认为 0。
+- shadowColor = color
+shadowColor 是标准的 CSS 颜色值，用于设定阴影颜色效果，默认是全透明的黑色。
+> 不受变换矩阵影响
+
+```js
+function draw() {
+  var ctx = document.getElementById('canvas').getContext('2d');
+
+  ctx.shadowOffsetX = 2;
+  ctx.shadowOffsetY = 2;
+  ctx.shadowBlur = 2;
+  ctx.shadowColor = "rgba(0, 0, 0, 0.5)";
+ 
+  ctx.font = "20px Times New Roman";
+  ctx.fillStyle = "Black";
+  ctx.fillText("Sample String", 5, 30);
+}
+```
+![shadow效果图](https://raw.githubusercontent.com/LiXiaoRan/PicGoBed/master/img/20191205160021.png)
